79cab470426fde6a4e974a793ac74bc61c503fd3
Nidhi kumari <115169748+singhnidhi1805@users.noreply.github.com>
2025-03-03 20:28:56 +0530

    Update swagger.js

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -42,7 +42,7 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: '/api',
+      url: 'https://shayakbackend-production.up.railway.app/api',
       description: 'Development server',
     },
   ],
